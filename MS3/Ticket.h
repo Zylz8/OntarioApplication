c9d7a97d666cc5b8ac8b4659cc7fe3a6f3489e85
ZhouJ <164371491+Zylz8@users.noreply.github.com>
2025-01-15 20:31:09 -0500

    Add files via upload

---
+++ MS3/Ticket.h
@@ -0,0 +1,37 @@
+/*
+Name: Jackey Zhou
+Student ID: 164431223
+Email: jzhou200@myseneca.ca
+Section: NBB
+
+Declaration:
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+*/
+#ifndef SENECA_TICKET_H_
+#define SENECA_TICKET_H_
+#include <iostream>
+#include "Time.h"
+#include "IOAble.h"
+namespace seneca {
+   class Ticket:public IOAble{
+      Time m_time;
+      int m_number;
+   public:
+       void setNumber(int number) {
+           m_number = number;
+       }
+      Ticket(int number);
+
+      void setTime(Time time) {
+          m_time = time;
+      }
+
+      Time time()const;
+      int number()const;
+      void resetTime();
+      std::ostream& write(std::ostream& ostr )const;
+      std::istream& read(std::istream& istr);
+   };
+}
+#endif // !SENECA_TICKET_H_
---
+++ MS3/Utils.cpp
@@ -0,0 +1,79 @@
+/***********************************************************************
+// OOP244 Project
+//
+// File  Utils.cpp
+// Author  Fardad Soleimanloo
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name    Jackey Zhou             Date            Reason
+***********************************************************************/
+
+/*
+Name: Jackey Zhou
+Student ID: 164431223
+Email: jzhou200@myseneca.ca
+Section: NBB
+
+Declaration:
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Utils.h"
+#include "Time.h"
+using namespace std;
+namespace seneca {
+    // start provided code
+    bool debug = false;
+    Utils U;
+    int Utils::getTime() {
+        int mins = -1;
+        if (debug) {
+            int duration[]{ 3,5,9,20,30 };
+            mins = (m_testMins %= 1440);
+            m_testMins += duration[m_testIndex++ % 5];
+        }
+        else {
+            time_t t = time(NULL);
+            tm lt = *localtime(&t);
+            mins = lt.tm_hour * 60 + lt.tm_min;
+        }
+        return mins;
+    }
+    void Utils::setDebugTime(int hour, int min) {
+        seneca::debug = true;
+        m_testMins = hour * 60 + min;
+    }
+
+    int Utils::strcmp(const char* s1, const char* s2)const {
+        int i;
+        for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++);
+        return s1[i] - s2[i];
+    }
+    // end provided code
+    void copyString(const char* source, char* destination) {
+        while (*source != '\0') {
+            *destination = *source;
+            ++source;
+            ++destination;
+        }
+        *destination = '\0';
+    }
+
+    char* my_strcpy(char* destination, const char* source) {
+        char* destination_ptr = destination;
+
+        while (*source != '\0') {
+            *destination_ptr++ = *source++;
+        }
+
+        *destination_ptr = '\0';
+
+        return destination;
+    }
+}
